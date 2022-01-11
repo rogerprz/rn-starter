a99d76b2134ce8e7807ffd4c83b548c3f9af4367
Roger Perez <rogerprz@github.com>
2022-01-10 22:48:10 -0800

    add buttons and navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hello Home Screen!</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hello Home Screen!</Text>
+      <Button
+        onPress={() => navigation.navigate('Components')} 
+        title="Components" 
+      />
+       <Button
+        onPress={() => navigation.navigate('Home')} 
+        title="Home" 
+      />
+       <Button
+        onPress={() => navigation.navigate('List')} 
+        title="Go To List" 
+      />
+      <TouchableOpacity onPress={() => navigation.navigate('List')}>
+        <Text>List Opacity</Text>
+        </TouchableOpacity >
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
